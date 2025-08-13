be6bdcb5180ec433fb8d4eb8e7c73b2ea9069d07
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2025-08-13 12:02:03 +0200

    account admin

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("email","password","role","full_name","phone")}),
+        ("Permissions", {"fields": ("is_active","is_staff","is_superuser","groups","user_permissions")}),
+        ("Important dates", {"fields": ("last_login","date_joined")}),
+    )
+    add_fieldsets = ((None, {"classes": ("wide",),"fields": ("email","password1","password2","role")}),)
+    list_display = ("email","role","is_active")
+    ordering = ("email",)
+    search_fields = ("email","full_name")
