bb2b17dafc62013a4f39cca2d542eb189b01ff20
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2025-08-14 08:21:34 +0200

    update

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -114,6 +114,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
